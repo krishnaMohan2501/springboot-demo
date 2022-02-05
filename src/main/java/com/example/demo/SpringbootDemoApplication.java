2c01897e93a4f93652d89809a5eb7703dd95500e
krishna <kykrishna24@gmail.com>
2022-02-06 00:26:31 +0530

    added helm chart and  | code refactoring

--- src/main/java/com/example/demo/SpringbootDemoApplication.java
+++ src/main/java/com/example/demo/SpringbootDemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,11 +10,6 @@ public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
-	}
-
-	@RequestMapping("/hello")
-	public String sayHello(){
-		return "Hello Spring Boot Application";
 	}
 
 }
